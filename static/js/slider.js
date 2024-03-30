b9d0c6b65770c2aef963d525cd4c01a767dba1bf
Dilraj Singh <dilraj082@gmail.com>
2024-03-30 22:56:41 +0530

    modal is not working yet

--- static/js/slider.js
+++ static/js/slider.js
@@ -25,3 +25,17 @@ document.addEventListener("DOMContentLoaded", function() {
 window.onload = function() {
   document.getElementById("background-audio").play();
 }
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
